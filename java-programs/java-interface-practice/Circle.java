06a7fbbf0ff0652de54c6e0eb0c66e8388c762d8
john <ralphesleyer@gmail.com>
2023-05-25 23:46:36 +0800

    added java-interface-practice

---
+++ java-programs/java-interface-practice/Circle.java
@@ -0,0 +1,17 @@
+public class Circle implements Shape{
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    @Override 
+    public void draw(){
+        System.out.println("Drawing a circle with radius " + radius);
+    }
+
+    @Override 
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+}
---
+++ java-programs/java-interface-practice/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(10, 20);
+        
+        circle.draw();
+        rectangle.draw();
+    }
+}
